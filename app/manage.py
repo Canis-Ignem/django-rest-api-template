2557902ca3415c95010ce9dd6c76b55436d5b0c0
Canis Ignem <36197823+Canis-Ignem@users.noreply.github.com>
2022-09-28 16:03:13 +0200

    added command to wait for the database to be ready

--- app/manage.py
+++ app/manage.py
@@ -15,21 +15,6 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    
-    AUTH_PASSWORD_VALIDATORS = [
-        {
-            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-        },
-    ]
     execute_from_command_line(sys.argv)
 
 
